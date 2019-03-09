fcb01ea4f8a81c20248f18ee00967735d3ab64b1
Till Dettmering <tilldettmering@gmail.com>
2019-03-09 07:59:22 +0000

    made a draw function

--- draw.py
+++ draw.py
@@ -1,25 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
 import epd2in7
-import time
 from PIL import Image,ImageDraw,ImageFont
-import traceback
+import sys
 
-try:
+def draw(imgfile):
     epd = epd2in7.EPD()
     epd.init()
     epd.Clear(0xFF)
     
     Himage = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)  # 255: clear the frame
     
-    print("read bmp file")
-    Himage = Image.open('2in7.bmp')
+    Himage = Image.open(imgfile)
     epd.display(epd.getbuffer(Himage))
         
     epd.sleep()
-        
-except:
-    print('traceback.format_exc():\n%s',traceback.format_exc())
-    exit()
 
+if __name__ == '__main__':
+    draw(sys.argv[1])
