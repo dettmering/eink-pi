87b302777190049f52e50f6d98609bd59fdbeaea
Till Dettmering <tilldettmering@gmail.com>
2019-03-09 08:14:38 +0000

    remove weather-related stuff from scripts

--- generate-svg.sh
+++ generate-svg.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-cd "$(dirname "$0")"
-
-python3 weather-script.py
-rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
-pngcrush -c 0 -ow weather-script-output.png
-cp -f weather-script-output.png /path/to/web/server/directory/weather-script-output.png
+python3 generate-svg.py
+rsvg-convert --background-color=white -o screen-output.png screen-template.svg
