cae0bcd70320307d9aec522605f4a03fb76f4242
Till Dettmering <tilldettmering@gmail.com>
2019-03-09 08:55:29 +0000

    output bmps

--- generate-svg.sh
+++ generate-svg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python3 generate-svg.py
-rsvg-convert --background-color=white -o screen-output.png screen-template.svg
+rsvg-convert --background-color=white -o screen-output.bmp screen-template.svg
